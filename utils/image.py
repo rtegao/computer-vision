876d8160f4a2d51642fd2b2b019382ad04bb9c4f
Tegao <rictegao@gmail.com>
2022-08-13 15:04:40 -0300

    refacor: class inheritance

--- utils/image.py
+++ utils/image.py
@@ -2,9 +2,23 @@ from logging import raiseExceptions
 import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy
+class Image:
+    def __init__(self):
+        pass
+    
+    def image_plot(self, image: numpy.array, title:str):
+        plt.title(title)
+        if len(image.shape) == 2:
+            plt.imshow(image, cmap="gray")
+        else:
+            plt.imshow(image)
 
-class ImageReader:
+    def image_shape(self, image: numpy.array):
+        return image.shape
+
+class ImageReader(Image):
     def __init__(self, image_path:str):
+        super().__init__()     
         self.image_path = image_path
 
     def read_image(self) -> numpy.ndarray:
@@ -16,21 +30,15 @@ class ImageReader:
             r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
             gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
             return gray
-        else:
-            ValueError('Image number of chanels is different than expected')
-
-
-
-class Image():
+        ValueError('Image number of chanels is different than expected')
+            
+class ImageTranformer(Image):
     def __init__(self, image:numpy.ndarray):
         self.image = image
     
-    def image_plot(self, title:str):
-        plt.title(title)
-        if len(self.image.shape) == 2:
-            plt.imshow(self.image, cmap="gray")
-        else:
-            plt.imshow(self.image)
-
-    def image_shape(self):
-        return self.image.shape
+    def negative(self):
+        w,h = self.image.shape
+        for i in range(w):
+            for j in range(h):
+                self.image[i,j] = 255 - self.image[i,j]
+        return self.image
